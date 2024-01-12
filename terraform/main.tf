40d52177b47740865493fbf3246e32469ff64664
David Hickey <dhickey@socialbridgetec.com>
2024-01-12 08:29:35 -0600

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "dhickey+2@socialbridgetec.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "dhickey+1@socialbridgetec.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
